2c0758a2635795eeddeaac224c1cae51c5734705
Fareed Tahir <fareedTahir131@gmail.com>
2026-02-02 14:19:47 +0500

    scan surface device anim fixed

--- Assets/PlaceOnPlane.cs
+++ Assets/PlaceOnPlane.cs
@@ -6,99 +6,163 @@ using UnityEngine.XR.ARSubsystems;
 
 namespace UnityEngine.XR.ARFoundation.Samples
 {
-    /// <summary>
-    /// Listens for touch events and performs an AR raycast from the screen touch point.
-    /// AR raycasts will only hit detected trackables like feature points and planes.
-    ///
-    /// If a raycast hits a trackable, the <see cref="placedPrefab"/> is instantiated
-    /// and moved to the hit position.
-    /// </summary>
     [RequireComponent(typeof(ARRaycastManager))]
     [RequireComponent(typeof(ARPlaneManager))]
     public class PlaceOnPlane : MonoBehaviour
     {
-        private ARPlaneManager PlaneManager;
-        [SerializeField]
-        [Tooltip("Instantiates this prefab on a plane at the touch location.")]
-        GameObject m_PlacedPrefab;
+        [Header("Placement")]
+        [SerializeField] private GameObject m_PlacedPrefab;
+        [SerializeField] private GameObject placementIndicator;
+
+        [Header("UI")]
+        [SerializeField] private Canvas coachingCanvas;
+        [SerializeField] private GameObject scanningUI;     // Screen 1
+        [SerializeField] private GameObject tapToPlaceUI;   // Screen 2
+        [SerializeField] private GameObject[] otherScreens; // Enable after placement
+
+        [Header("Events")]
         public UnityEvent onContentPlaced;
 
-        /// <summary>
-        /// The prefab to instantiate on touch.
-        /// </summary>
-        public GameObject placedPrefab
-        {
-            get { return m_PlacedPrefab; }
-            set { m_PlacedPrefab = value; }
-        }
-        /// <summary>
-        /// The object instantiated as a result of a successful raycast intersection with a plane.
-        /// </summary>
         public GameObject spawnedObject { get; private set; }
+
+        private ARRaycastManager raycastManager;
+        private ARPlaneManager planeManager;
+
+        private static readonly List<ARRaycastHit> hits = new();
+
+        private bool planeDetected;
+        private bool placementPoseValid;
+
+        private Pose placementPose;
 
         void Awake()
         {
-            m_RaycastManager = GetComponent<ARRaycastManager>();
-            gameObject.GetComponent<ARPlaneManager>().enabled = true;
-            PlaneManager = GetComponent<ARPlaneManager>();
+            raycastManager = GetComponent<ARRaycastManager>();
+            planeManager = GetComponent<ARPlaneManager>();
+
+            placementIndicator.SetActive(false);
+
+            scanningUI.SetActive(true);
+            tapToPlaceUI.SetActive(false);
+
+            foreach (var screen in otherScreens)
+                screen.SetActive(false);
         }
 
-        bool TryGetTouchPosition(out Vector2 touchPosition)
+        void OnEnable()
         {
-    #if UNITY_EDITOR
-            if (Input.GetMouseButton(0))
-            {
-                var mousePosition = Input.mousePosition;
-                touchPosition = new Vector2(mousePosition.x, mousePosition.y);
-                return true;
-            }
-    #else
-            if (Input.touchCount > 0)
-            {
-                touchPosition = Input.GetTouch(0).position;
-                return true;
-            }
-    #endif
+            planeManager.planesChanged += OnPlanesChanged;
+        }
 
-            touchPosition = default;
-            return false;
+        void OnDisable()
+        {
+            planeManager.planesChanged -= OnPlanesChanged;
+        }
+
+        private void OnPlanesChanged(ARPlanesChangedEventArgs args)
+        {
+            if (!planeDetected && planeManager.trackables.count > 0)
+            {
+                planeDetected = true;
+
+                scanningUI.SetActive(false);
+                tapToPlaceUI.SetActive(true);
+
+                placementIndicator.SetActive(true);
+            }
         }
 
         void Update()
         {
-            if (!TryGetTouchPosition(out Vector2 touchPosition))
+            if (spawnedObject != null)
                 return;
 
-            if (m_RaycastManager.Raycast(touchPosition, s_Hits, TrackableType.PlaneWithinPolygon))
-            {
-                // Raycast hits are sorted by distance, so the first one
-                // will be the closest hit.
-                var hitPose = s_Hits[0].pose;
-                
-                if (spawnedObject == null)
-                {
-                    spawnedObject = Instantiate(m_PlacedPrefab, hitPose.position, hitPose.rotation);
-                    DisableAllPlanes();
+            UpdatePlacementIndicator();
 
-                    onContentPlaced.Invoke();
-                    Handheld.Vibrate();
-                    gameObject.GetComponent<ARPlaneManager>().enabled = false;
-                }
-                //else
-                //{
-                //    spawnedObject.transform.position = hitPose.position;
-                //}
+            if (!placementPoseValid)
+                return;
+
+            if (TryGetTouch(out _))
+            {
+                PlaceObject();
             }
         }
+
+        // -------------------------------------
+
+        private void UpdatePlacementIndicator()
+        {
+            Vector2 screenCenter = new(Screen.width / 2f, Screen.height / 2f);
+
+            if (raycastManager.Raycast(screenCenter, hits, TrackableType.PlaneWithinPolygon))
+            {
+                placementPoseValid = true;
+                placementPose = hits[0].pose;
+
+                placementIndicator.transform.SetPositionAndRotation(
+                    placementPose.position,
+                    placementPose.rotation);
+            }
+            else
+            {
+                placementPoseValid = false;
+                placementIndicator.SetActive(false);
+            }
+        }
+
+        // -------------------------------------
+
+        bool TryGetTouch(out Vector2 pos)
+        {
+#if UNITY_EDITOR
+            if (Input.GetMouseButtonDown(0))
+            {
+                pos = Input.mousePosition;
+                return true;
+            }
+#else
+            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                pos = Input.GetTouch(0).position;
+                return true;
+            }
+#endif
+
+            pos = default;
+            return false;
+        }
+
+        // -------------------------------------
+
+        private void PlaceObject()
+        {
+            spawnedObject = Instantiate(
+                m_PlacedPrefab,
+                placementIndicator.transform.position,
+                placementIndicator.transform.rotation);
+
+            DisableAllPlanes();
+
+            coachingCanvas.gameObject.SetActive(false);
+
+            foreach (var screen in otherScreens)
+                screen.SetActive(true);
+
+            placementIndicator.SetActive(false);
+
+            onContentPlaced?.Invoke();
+
+            Handheld.Vibrate();
+
+            planeManager.enabled = false;
+        }
+
+        // -------------------------------------
+
         private void DisableAllPlanes()
         {
-            foreach (var plane in PlaneManager.trackables)
-            {
+            foreach (var plane in planeManager.trackables)
                 plane.gameObject.SetActive(false);
-            }
         }
-        static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
-
-        ARRaycastManager m_RaycastManager;
     }
 }
